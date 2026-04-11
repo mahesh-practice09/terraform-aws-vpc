356563772598fe326b8ef192481ef13024350bdc
mahesh.ganji <mahesh050917@gmail.com>
2026-04-11 07:07:53 +0530

    terraform vpc module

--- variables.tf
+++ variables.tf
@@ -1,15 +1,14 @@
 variable "cidr_block" {
-   default = "10.0.0.0/16"
    type = string
 }
 
 variable "Project" {
     type = string
-    default = "Roboshop"
+   
 }
 
 variable "Env" {
     type = string
-    default = "dev"
+  
 }
 
