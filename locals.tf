e0e912db959dfa4c72300f6ae2ae9144e0041fba
mahesh.ganji <mahesh050917@gmail.com>
2026-04-11 07:49:19 +0530

    terraform vpc module

--- locals.tf
+++ locals.tf
@@ -1,5 +1,10 @@
 locals {
-    common_tags = {
+   
+    public_cidr_blocks = [ "10.0.1.0/24","10.0.2.0/24"]
+    private_cidr_blocks = [ "10.0.11.0/24","10.0.12.0/24"]
+    db_private_cidr_blocks = [ "10.0.21.0/24","10.0.22.0/24"]
+
+      common_tags = {
         Project = var.Project
         Terraform = true
         Env = var.Env
@@ -8,11 +13,13 @@ locals {
         Name = "${var.Project}-${var.Env}-vpc"
     }
     vpc_tags = merge(local.common_tags,local.vpc_final_tags)
+
+    selected_zones = slice(data.aws_availability_zones.available.names,0,2)
     
     igw_final_tags = {
         Name = "${var.Project}-${var.Env}-igw"
     }
     
     igw_tags = merge(local.common_tags,local.igw_final_tags)
-
+   
 }
--- main.tf
+++ main.tf
@@ -10,12 +10,36 @@ resource "aws_internet_gateway" "main" {
   tags = local.igw_tags
 }
 
-# resource "aws_subnet" "main" {
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = "10.0.1.0/24"
 
-#   tags = {
-
-#   }
-       
+ resource "aws_subnet" "public_subnet" {
+  count = var.public_cidr_blocks
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_cidr_blocks[count.index]
   
+  tags = {
+      Name = "${var.Project}-${var.Env}-public-snet-${local.selected_zones[count-index]}"
+  }
+ 
+}      
+  
+ resource "aws_subnet" "private_subnet" {
+  count = var.private_cidr_blocks
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_cidr_blocks[count.index]
+  
+  tags = {
+      Name = "${var.Project}-${var.Env}-private-snet-${local.selected_zones[count-index]}"
+  }
+ 
+}      
+
+ resource "aws_subnet" "db_private_subnet" {
+  count = var.db_private_cidr_blocks
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.db_private_cidr_blocks[count.index]
+  
+  tags = {
+      Name = "${var.Project}-${var.Env}-db-private-snet-${local.selected_zones[count-index]}"
+  }
+ 
+}  
