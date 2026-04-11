50fb6e60854878c4691bb7a259567275437aa903
mahesh.ganji <mahesh050917@gmail.com>
2026-04-11 07:11:15 +0530

    terraform vpc module

--- locals.tf
+++ locals.tf
@@ -7,6 +7,6 @@ locals {
     vpc_final_tags = {
         Name = "${var.Project}-${var.Env}-vpc"
     }
-    vpc_tags = merge(local.common_tags,vpc_tags)
+    vpc_tags = merge(local.common_tags,local.vpc_final_tags)
    
 }
