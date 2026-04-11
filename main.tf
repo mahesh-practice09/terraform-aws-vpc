a641625f4217845b4c9d9b701f513adbb2a32380
mahesh.ganji <mahesh050917@gmail.com>
2026-04-11 07:33:41 +0530

    terraform vpc module

--- main.tf
+++ main.tf
@@ -9,3 +9,13 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
   tags = local.igw_tags
 }
+
+# resource "aws_subnet" "main" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.1.0/24"
+
+#   tags = {
+
+#   }
+       
+  
