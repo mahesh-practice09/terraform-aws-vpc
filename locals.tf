d8e13aefb82af631e2207d4c927ccbce5e9ef881
mahesh.ganji <mahesh050917@gmail.com>
2026-04-11 07:10:23 +0530

    terraform vpc module

--- locals.tf
+++ locals.tf
@@ -1,12 +1,11 @@
 locals {
-    env = var.env
     common_tags = {
         Project = var.Project
         Terraform = true
-        Env = var.env
+        Env = var.Env
     }
     vpc_final_tags = {
-        Name = "${var.Project}-${var.env}-vpc"
+        Name = "${var.Project}-${var.Env}-vpc"
     }
     vpc_tags = merge(local.common_tags,vpc_tags)
    
