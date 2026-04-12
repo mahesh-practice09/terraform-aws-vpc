3cb517dfb5e6cf7ea9165b9236f1de15a28ac21b
mahesh.ganji <mahesh050917@gmail.com>
2026-04-12 06:57:30 +0530

    terraform vpc module

--- peering.tf
+++ peering.tf
@@ -11,3 +11,31 @@ resource "aws_vpc_peering_connection" "main" {
     allow_remote_vpc_dns_resolution = true
   }
 }
+
+resource "aws_route" "public_peering" {
+   count = var.is_peering_required ? 1: 0
+   route_table_id = aws_route_table.public_rtable.id
+   destination_cidr_block = data.aws_vpc.default.cidr_block
+   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
+
+resource "aws_route" "private_peering" {
+   count = var.is_peering_required ? 1: 0
+   route_table_id = aws_route_table.private_rtable.id
+   destination_cidr_block = data.aws_vpc.default.cidr_block
+   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
+
+resource "aws_route" "db_private_peering" {
+   count = var.is_peering_required ? 1: 0
+   route_table_id = aws_route_table.db_private_rtable
+   destination_cidr_block = data.aws_vpc.default.cidr_block
+   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
+
+resource "aws_route" "default_route_peering" {
+   count = var.is_peering_required ? 1: 0
+   route_table_id = data.aws_route_table.default.id
+   destination_cidr_block = aws_vpc.main.cidr_block
+   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
