e57c1c542db0ba97757814c763e5428f3e3ad898
mahesh.ganji <mahesh050917@gmail.com>
2026-04-12 07:23:43 +0530

    terraform vpc module

--- main.tf
+++ main.tf
@@ -122,13 +122,13 @@ resource "aws_route_table_association" "public_rt_assoc" {
 
 resource "aws_route_table_association" "private_rt_assoc" {
   count = length(aws_subnet.private_subnet)
-  subnet_id      = aws_subnet.public_subnet[count.index].id
+  subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.private_rtable.id
 }
 
 
 resource "aws_route_table_association" "db_private_rt_assoc" {
   count = length(aws_subnet.db_private_subnet)
-  subnet_id      = aws_subnet.public_subnet[count.index].id
+  subnet_id      = aws_subnet.db_private_subnet[count.index].id
   route_table_id = aws_route_table.db_private_rtable.id
 }
