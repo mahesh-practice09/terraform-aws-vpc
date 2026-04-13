15dd7be3829fbe232e9b5761d12dcd4e51e24c20
mahesh.ganji <mahesh050917@gmail.com>
2026-04-13 16:14:05 +0530

    terraform vpc module

--- outputs.tf
+++ outputs.tf
@@ -7,13 +7,13 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public_subnet.id
+    value = aws_subnet.public_subnet[*].id
 }
 
 output "private_subnet_ids" {
-  value = aws_subnet.private_subnet.id
+  value = aws_subnet.private_subnet[*].id 
 }
 
 output "db_private_subnet_ids" {
-  value = aws_subnet.db_private_subnet.id
+  value = aws_subnet.db_private_subnet[*].id
 }
